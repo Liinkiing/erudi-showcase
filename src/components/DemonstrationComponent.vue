eda6316ec3f17766b06f1901df508d89ab43ff96
Omar Jbara <omar.Jbara2@gmail.com>
2018-06-14 01:31:08 +0200

    Added demo iframe

--- src/components/DemonstrationComponent.vue
+++ src/components/DemonstrationComponent.vue
@@ -3,7 +3,9 @@
     <div class="container">
       <h2>Démonstration</h2>
 
-      <div class="demonstration-iframe iframe"></div>
+      <div class="demonstration-iframe iframe">
+        <iframe src="https://player.vimeo.com/video/274980795" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+      </div>
     </div>
 
     <img src="../assets/img/demonstration/illustration.svg" alt="Illustration">
@@ -23,16 +25,23 @@ export default {
   .demonstration {
     text-align: center;
     background-color: $sky-color;
+    & h2 {
+      padding-top: 40px;
+    }
 
     &-iframe {
       position: relative;
       margin-top: 8rem;
-
+      width: 100%;
+      height: 64rem;
+      & iframe {
+        width: 100%;
+      }
       &::after {
         content: url("../assets/img/demonstration/mountains.png");
         position: absolute;
-        top: 0;
-        right: -.6rem;
+        top: -1px;
+        right: 1.5rem;
         transform: translateY(-98%);
       }
     }
