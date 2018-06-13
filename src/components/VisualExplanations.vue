694e204a1b4faeaa34122006e2235b527a600960
Omar Jbara <omar.Jbara2@gmail.com>
2018-06-14 01:01:48 +0200

    Added carnet, fiche and recette

--- src/components/VisualExplanations.vue
+++ src/components/VisualExplanations.vue
@@ -10,20 +10,30 @@
         <p>A chaque pays sa senteur. Pendant la lecture garde pres de toi ton diffuseur. Il te fera voyager en propageant une odeur.</p>
       </div>
 
-      <div class="text-group">
-        <h2>Un carnet d'exploration</h2>
-        <p>Dans ton carnet d’exploration tu observera ton évolution !</p>
+      <div class="row">
+        <div class="text-group">
+          <h2>Un carnet d'exploration</h2>
+          <p>Dans ton carnet d’exploration tu observera ton évolution !</p>
+        </div>
+        <img src="../assets/img/visual-explanations/carnet.png" alt="Carnet d'exploration">
       </div>
 
-      <div class="text-group">
-        <h2>Une fiche informative</h2>
-        <p>Approfondi tes connaissances sur les pays visités. Découvre sa faune et sa flore !</p>
+      <div class="row inversed">
+        <div class="text-group">
+          <h2>Une fiche informative</h2>
+          <p>Approfondi tes connaissances sur les pays visités. Découvre sa faune et sa flore !</p>
+        </div>
+        <img src="../assets/img/visual-explanations/fiche.png" alt="Informations pays">
       </div>
 
-      <div class="text-group">
-        <h2>Une fiche recette</h2>
-        <p>Découvre le goût de l’odeur à travers une recette typique du pays choisi.</p>
+      <div class="row">
+        <div class="text-group">
+          <h2>Une fiche recette</h2>
+          <p>Découvre le goût de l’odeur à travers une recette typique du pays choisi.</p>
+        </div>
+        <img src="../assets/img/visual-explanations/recette.png" alt="Informations recettes">
       </div>
+
 
     </div>
   </div>
@@ -45,7 +55,18 @@ export default {
 
   .visual-explanations {
     background-color: $visual-color;
-
+    padding: 60px;
+    .row {
+      margin: 40px 0;
+      display: flex;
+      align-items: center;
+      &.inversed {
+        flex-direction: row-reverse;
+      }
+      img {
+        width: 729px;
+      }
+    }
     .text-group {
       text-align: left;
     }
